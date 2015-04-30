259e805814d337e72f2723f10297346026b7c2f6
kingofcrabs <rex.xie@qq.com>
2015-04-30 10:47:52 +0800

    init

---
+++ FillBarcodeVolume/FillBarcodeVolume/ExcelFile.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace FillBarcodeVolume
+{
+    class ExcelFile
+    {
+        public void WriteResult( List<VolumeInfo> allVolumeInfos, string sFile)
+        {
+
+            Application excelApp = new Application();
+            Workbook workBook = excelApp.Workbooks.Add();
+
+            Worksheet xlsWs = null;
+            Range ExcelCellText;
+            workBook.Worksheets.Add();
+            xlsWs = (Worksheet)workBook.Worksheets.get_Item(1);
+            string startCell = ConfigurationManager.AppSettings["startCell"];
+            ExcelCellText = xlsWs.get_Range("startCell", Missing.Value);
+            ExcelCellText = ExcelCellText.get_Resize(allVolumeInfos.Count, 3);
+            string[,] myArray = new string[allVolumeInfos.Count, 3];
+            for (int r = 0; r < allVolumeInfos.Count; r++)
+            {
+                myArray[r, 0] = allVolumeInfos[r].orgBarcode;
+                myArray[r, 1] = allVolumeInfos[r].resBarcode;
+                myArray[r, 2] = allVolumeInfos[r].volume;
+            }
+
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+            ExcelCellText.set_Value(Missing.Value, myArray);
+            workBook.SaveAs(sFile);
+            workBook.Close();
+            excelApp.Quit();
+        }
+    }
+
+    class VolumeInfo
+    {
+        public string orgBarcode;
+        public string resBarcode;
+        public string volume;
+    }
+}
---
+++ FillBarcodeVolume/FillBarcodeVolume/VolumeFile.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace FillBarcodeVolume
+{
+    class VolumeFile
+    {
+        int m_Slices = 0;
+        public List<string> Parse(IEnumerable<string> lines)
+        {
+            if (lines.Count() % m_Slices != 0)
+            {
+                throw new Exception("incorrect line count, should be: " + m_Slices.ToString());
+            }
+
+            List<string> allVolumes = new List<string>();
+            while(lines.Count() > 0)
+            {
+                var thisBatchLines = lines.Take(m_Slices);
+                lines = lines.Skip(m_Slices);
+                allVolumes.AddRange(GetVolThisRegion(thisBatchLines));
+            }
+            return allVolumes;
+        }
+
+        private IEnumerable<string> GetVolThisRegion(IEnumerable<string> thisBatchLines)
+        {
+            List<List<string>> volumes = new List<List<string>>();
+            foreach(string line in thisBatchLines)
+            {
+                volumes.Add(line.Split(',').Select(x => ConvertFormat(x)).ToList());
+            }
+            int nSlices = volumes.Count;
+            int batchTips = volumes[0].Count;
+            List<string> result = new List<string>();
+            for( int tipIndex = 0; tipIndex < batchTips; tipIndex++)
+            {
+                for(int i = 0; i< nSlices; i++)
+                {
+                    result.Add(volumes[i][tipIndex]);
+                }
+            }
+            return result;
+        }
+
+        private string ConvertFormat(string x)
+        {
+            int dotIndex = x.IndexOf('.');
+            return x.Substring(0, dotIndex);
+        }
+
+        public IEnumerable<string> Read(string file, int slices)
+        {
+            m_Slices = slices;
+            var lines = File.ReadAllLines(file);
+            lines = lines.Skip(1).ToArray();
+            return lines;
+        }
+    }
+}
