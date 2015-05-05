77c12b7e2e63385dbb0e30e155a39220b1df17cf
kingofcrabs <rex.xie@qq.com>
2015-05-05 09:19:25 +0800

    create.

--- FillBarcodeVolume/FillBarcodeVolume/Program.cs
+++ FillBarcodeVolume/FillBarcodeVolume/Program.cs
@@ -2,37 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows;
 
 namespace FillBarcodeVolume
 {
     class Program
     {
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        [STAThread]
         static void Main(string[] args)
         {
             try
             {
-                FillInfo();
+                var form = new  FillBarcodeVolume();
+                form.ShowDialog();
             }
             catch(Exception ex)
             {
                 log.Error(ex.Message + ex.StackTrace);
             }
-            Console.WriteLine("Press any key to continue...");
-            Console.ReadKey();
         }
 
-        private static void FillInfo()
-        {
-            BarcodesFile barcodesFile = new BarcodesFile();
-            int slices = 0;
-            string barcodeFile = @"F:\Projects\MergeResult.git\trunk\data\防错1.csv";
-            log.InfoFormat("Read file: {0}", barcodeFile);
-            var barcodes = barcodesFile.Read(barcodeFile, ref slices);
-            VolumeFile volumeFile = new VolumeFile();
-            string volumeFilePath = @"F:\Projects\MergeResult.git\trunk\data\体积数据1.csv";
-            var lines = volumeFile.Read(volumeFilePath, slices);
-            volumeFile.Parse(lines);
-        }
     }
 }
